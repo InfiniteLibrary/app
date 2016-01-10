7be4c463a47907d5252e0d97bc5a324553e7c567
Eric Gardner <gardner.ec@gmail.com>
2016-01-10 12:08:14 -0500

    Add new components

---
+++ source/javascripts/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<style lang="scss" scoped>
+// Component styles
+.header {
+  .logo {
+    display: inline;
+  }
+
+  h1 {
+    display: inline;
+  }
+}
+</style>
+
+<template>
+<header-banner></header-banner>
+<header-search></header-search>
+<header-nav></header-nav>
+</template>
+
+<script>
+import HeaderBanner from "./HeaderBanner.vue";
+import HeaderSearch from "./HeaderSearch.vue";
+import HeaderNav from "./HeaderNav.vue";
+
+export default {
+  components: {
+    HeaderBanner,
+    HeaderSearch,
+    HeaderNav
+  }
+}
+</script>
