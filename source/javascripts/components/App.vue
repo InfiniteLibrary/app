79bc71d9b43e43b961b59fd24abc4fc84d93f65a
Eric Gardner <gardner.ec@gmail.com>
2016-01-09 20:30:41 -0500

    Add basic search component

--- source/javascripts/components/App.vue
+++ source/javascripts/components/App.vue
@@ -1,16 +1,19 @@
 <template lang="jade">
 #app
   app-bar(msg = "This is passed from the parent")
+  search
   app-content
 </template>
 
 <script>
 import AppBar from "./AppBar.vue";
 import AppContent from "./AppContent.vue";
+import Search from "./Search.vue";
 
 export default {
   components: {
     AppBar,
+    Search,
     AppContent
   }
 }
---
+++ source/javascripts/components/Search.vue
@@ -0,0 +1,16 @@
+<template lang="jade">
+#search
+  input(v-model="query")
+  p {{ query }}
+</template>
+
+<script>
+export default {
+  data: {
+    query: ''
+  },
+  methods: {
+
+  }
+}
+</script>
