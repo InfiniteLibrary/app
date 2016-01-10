2707b954f62b39cc2646fa9b9ddd9b2664168dbb
nelliemckesson <nellie.mckesson@macmillan.com>
2016-01-10 09:49:10 -0500

    removing nav from header

--- source/javascripts/components/AppBar.vue
+++ source/javascripts/components/AppBar.vue
@@ -1,23 +1,6 @@
 <style lang="scss" scoped>
 // Component styles
 .header {
-  .navbar {
-
-    border-bottom: 1px solid #333;
-
-    ul {
-      margin: 0;
-      padding: 0;
-      list-style-type: none;
-      list-style-position: inside;
-    }
-
-    li {
-      display: inline;
-    }
-
-  }
-
   .logo {
     display: inline;
   }
@@ -33,11 +16,6 @@
   <img class="logo"/>
   <h1>App Title</h1>
   <h2>App description</h2>
-  <ul class="navbar">
-    <li><a v-link="'home'">target</a></li>
-    <li><a v-link="'home'">target</a></li>
-    <li><a v-link="'home'">target</a></li>
-  </ul>
 </div>
 </template>
 
